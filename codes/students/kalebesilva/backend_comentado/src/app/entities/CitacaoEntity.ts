ca0c08a3d370c6c8439b3459b03f22a37517339d
Kalebe silva <kalebe.silvadev@gmail.com>
2023-06-15 16:49:03 -0500

    Entity, finish the comments

--- codes/students/kalebesilva/backend_comentado/src/app/entities/CitacaoEntity.ts
+++ codes/students/kalebesilva/backend_comentado/src/app/entities/CitacaoEntity.ts
@@ -1,22 +1,30 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, OneToMany } from 'typeorm';
 import ColecaoEntity from './ColecaoEntity';
 
-@Entity('citacao')
+@Entity('citacao') /*Essa classe irá servir para criacao da tabela no banco de dados
+a tela estará com o mesmos atributos listado a baixo, a forma que é escrita
+lembra bastante o hibernate com java */
 class CitacaoEntity {
-    @PrimaryGeneratedColumn('increment')
-    id?: number;
+    @PrimaryGeneratedColumn('increment')// Primary key, autoIncrement
+    id?: number;//Atributo ID, do tipo number
 
-    @Column({ length: 1000, type: 'varchar', nullable: false })
-    titulo: string;
+    @Column({ length: 1000, type: 'varchar', nullable: false }) /*
+    Coluna com tamanho máximo de 1000 caracteres, tipo varchar, not null */
+    titulo: string;// Campo titulo, tipo de dado: string
 
-    @CreateDateColumn({ name: 'created_at' })
-    created_at?: Date;
+    @CreateDateColumn({ name: 'created_at' }) //Coluna de data, salvando a data assim que um
+    // novo registro é criado
+    created_at?: Date; // campo data, informando quando foi criado  o registro
 
-    @Column({ name: 'id_colecao', type: 'int', nullable: false })
-    id_colecao: number;
+    @Column({ name: 'id_colecao', type: 'int', nullable: false }) /**
+    Coluna foreign key, referendo a id_colecao, tipo inteiro not null */
+    id_colecao: number; // campo id_colecao, tipo do dado: numero
 
-    @OneToMany(() => ColecaoEntity, (colecao) => colecao.citacoes)
-    colecao?: ColecaoEntity;
+    @OneToMany(() => ColecaoEntity, (colecao) => colecao.citacoes) /* Relacionamento um para muitos, referentes
+    as tabelas colecao, conectaca a citacao.
+    
+    */
+    colecao?: ColecaoEntity; // compo para relacao.
 }
 
-export default CitacaoEntity;
+export default CitacaoEntity; // exportando a classe para ser usada por outras classes
--- codes/students/kalebesilva/backend_comentado/src/app/entities/ColecaoEntity.ts
+++ codes/students/kalebesilva/backend_comentado/src/app/entities/ColecaoEntity.ts
@@ -1,28 +1,28 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, OneToMany } from 'typeorm';
 import CitacaoEntity from './CitacaoEntity';
 
-@Entity('colecao')
-class ColecaoEntity {
-    @PrimaryGeneratedColumn('increment')
-    id: number;
+@Entity('colecao') // Tabela colecao
+class ColecaoEntity { //Ela é composta pelos seguintes atributos
+    @PrimaryGeneratedColumn('increment') // Chave primaria, autoincrement
+    id: number;// Compo ID (chave primaria)
 
-    @Column({ length: 100, type: 'varchar', nullable: false })
-    titulo: string;
+    @Column({ length: 100, type: 'varchar', nullable: false })/*Titulo, campo com tamanho max de 100, tipo varchar, not null */
+    titulo: string;//campo tituto, tipo string
 
-    @Column({ length: 100, type: 'varchar', nullable: true })
-    subtitulo?: string;
+    @Column({ length: 100, type: 'varchar', nullable: true })/*Campo substitulo, tamanha 100 caracteres, tipo varchar, not null*/
+    subtitulo?: string; // campo substitulo
 
-    @Column({ type: 'varchar' })
-    imagem: string;
+    @Column({ type: 'varchar' }) // Coluna varchar, para guardar imagens
+    imagem: string; // image, type string
 
-    @Column({ length: 100, type: 'varchar', nullable: false })
-    autor: string;
+    @Column({ length: 100, type: 'varchar', nullable: false }) // Column with the size 100, your type is varchar and its not accept nullable field
+    autor: string; // autor field string type
 
-    @CreateDateColumn({ name: 'created_at' })
-    created_at?: Date;
+    @CreateDateColumn({ name: 'created_at' }) // date when you create this camp his save exact moment when its created,
+    created_at?: Date;// field name
 
-    @OneToMany(() => CitacaoEntity, (citacao) => citacao.colecao)
-    citacoes?: CitacaoEntity[]
+    @OneToMany(() => CitacaoEntity, (citacao) => citacao.colecao) // relation one to many with colecao
+    citacoes?: CitacaoEntity[] // colacao de itens citacoes, relacao com dupla relacao.
 }
 
-export default ColecaoEntity;
+export default ColecaoEntity; // export this classe for any class get this in your compose.
