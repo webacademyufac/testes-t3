7bf59c653f4ef0154571b287d55d4dcf86836036
Kalebe silva <kalebe.silvadev@gmail.com>
2023-06-16 14:27:17 -0500

    Comentários das interfaces entities

--- codes/students/kalebesilva/backend_comentado/src/app/interfaces/entities/CitacaoInterface.ts
+++ codes/students/kalebesilva/backend_comentado/src/app/interfaces/entities/CitacaoInterface.ts
@@ -1,8 +1,9 @@
-interface CitacaoInterface {
+/*Interface dos objetos, se fosse em java, poderiamos entender como se fosse uma classe Model */
+interface CitacaoInterface { 
   id?: number;
   titulo: string;
   id_colecao: number;
   created_at?: Date;
 }
 
-export default CitacaoInterface;
+export default CitacaoInterface; // Exportando a interface que que possa ser usada em outras partes da aplicacao.
--- codes/students/kalebesilva/backend_comentado/src/app/interfaces/entities/ColecaoInterface.ts
+++ codes/students/kalebesilva/backend_comentado/src/app/interfaces/entities/ColecaoInterface.ts
@@ -1,3 +1,5 @@
+/*Interface de colecao, ela funciona como se fosse o Model em que os objetos Colecao devem seguir
+, nesse caso, um obj colecao deve ter esses seguinte atributos */
 interface ColecaoInterface {
     id?: number;
     titulo: string;
@@ -6,4 +8,4 @@ interface ColecaoInterface {
     autor: string;
 }
 
-export default ColecaoInterface;
+export default ColecaoInterface; //Exportando a interface par ser usada em outras partes da aplicacao.
