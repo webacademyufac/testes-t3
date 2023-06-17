bfdb51411f800d31d5f18eb339e2992ec7677269
Kalebe silva <kalebe.silvadev@gmail.com>
2023-06-17 17:02:56 -0500

    comentários de repository

--- codes/students/kalebesilva/backend_comentado/src/app/repositories/CitacaoRepository.ts
+++ codes/students/kalebesilva/backend_comentado/src/app/repositories/CitacaoRepository.ts
@@ -4,14 +4,16 @@ import { AppDataSource } from "../../database/data-source";
 import { CitacaoRepositoryInterface } from "../interfaces/repositories/CitacaoRepositoryInterface";
 import { Repository } from "typeorm";
 
-class CitacaoRepository implements CitacaoRepositoryInterface {
-  private readonly citacaoRepository: Repository<CitacaoEntity>;
+class CitacaoRepository implements CitacaoRepositoryInterface { /*Classe citacao repositore que firma um contrato com 
+a interface citacaoRepository */
+  private readonly citacaoRepository: Repository<CitacaoEntity>; //Variável de leitura para ser usada ao decorrer da classe
 
-  constructor() {
+  constructor() { // construtor para injecao de dependencias
     this.citacaoRepository = AppDataSource.getRepository(CitacaoEntity)
   }
 
-  async findAll(titulo: string): Promise<CitacaoEntity[]> {
+  async findAll(titulo: string): Promise<CitacaoEntity[]> { // método FindAll, busca essa 
+    //query a seguir no banco de dados e retornar uma colecao de objetos.
     let query = `
     SELECT
       citacao.id,
@@ -28,15 +30,19 @@ class CitacaoRepository implements CitacaoRepositoryInterface {
       colecao ON citacao.id_colecao = colecao.id
   `;
     
-    if (titulo) {
+    if (titulo) { // verfica se é uma pesquisa por um titulo específico, se o método for
+      // chamado sem nenhum atributo, entao trará todos os registros citacao, mas se tiver um titulo, então
+      //retornara todas as citacoes que possuam aquele titulo.
       query += `WHERE citacao.titulo LIKE '%${titulo}%'`
     }
 
-    return this.citacaoRepository.query(query)
+    return this.citacaoRepository.query(query)// retorna a colecao de registros
   }
 
-  async findById(id: number): Promise<CitacaoEntity | null> {
-    const query = `
+  async findById(id: number): Promise<CitacaoEntity | null> { /*Método para buscar uma citacao em especifico no banco de dados,
+  o retorno dela pode ser null, caso não encontre a citacao ou o registro caso encontre. */
+  /*Query que irá ser utilizada para buscar a citacao */  
+  const query = ` 
     SELECT
       citacao.id,
       citacao.titulo as citacao_titulo,
@@ -53,27 +59,29 @@ class CitacaoRepository implements CitacaoRepositoryInterface {
     WHERE
       citacao.id = ${id}`;
   
-    const citacao = await this.citacaoRepository.query(query)
+    const citacao = await this.citacaoRepository.query(query) // salva a resposta em uma variável
 
-    return citacao.length ? citacao[0] : null
+    return citacao.length ? citacao[0] : null // verifica se ela é nula ou não, se for sim, retorna 
+    // o registro, se não, retorna null.
   }
 
-  async getCitacaoOnly(id: number): Promise<CitacaoEntity> {
-    return this.citacaoRepository.findOne({ where: { id } })
+  async getCitacaoOnly(id: number): Promise<CitacaoEntity> { //método para pegar apenas uma citacao
+    return this.citacaoRepository.findOne({ where: { id } })// Retorna apenas um registro e ele utiliza uma funcao do framework
   }
 
-  async create(colecao: CitacaoInterface): Promise<CitacaoEntity> {
-    return this.citacaoRepository.save(colecao)
+  async create(colecao: CitacaoInterface): Promise<CitacaoEntity> { // método para inserir um novo registro no banco de dados
+    return this.citacaoRepository.save(colecao)// returna o item salvo e para salvar, utilizar o método save do framework
   }
 
-  async update(id: number, colecao: CitacaoInterface): Promise<CitacaoEntity> {
-    await this.citacaoRepository.update(id, colecao)
-    return this.citacaoRepository.findOne({ where: { id } })
+  async update(id: number, colecao: CitacaoInterface): Promise<CitacaoEntity> {/*Método update, atualiza um registro no banco de dados */
+    await this.citacaoRepository.update(id, colecao)// espera realizar o update 
+    return this.citacaoRepository.findOne({ where: { id } }) // retorna ao usuário o registro que foi alterado,
+    //utilizando uma funcao de busca.
   }
 
-  async delete(id: number): Promise<void> {
-    await this.citacaoRepository.delete(id)
+  async delete(id: number): Promise<void> { // deleta um registro do banco de dados
+    await this.citacaoRepository.delete(id) //aguarda enquanto a operacao para deleta o registro está em andamento
   }
 }
 
-export default new CitacaoRepository()
+export default new CitacaoRepository() // exporta a classe para ser usada em outras partes da aplicacao.
--- codes/students/kalebesilva/backend_comentado/src/app/repositories/ColecaoRepository.ts
+++ codes/students/kalebesilva/backend_comentado/src/app/repositories/ColecaoRepository.ts
@@ -3,13 +3,14 @@ import ColecaoInterface from "../interfaces/entities/ColecaoInterface";
 import { AppDataSource } from "../../database/data-source";
 import { ColecaoRepositoryInterface } from "../interfaces/repositories/ColecaoRepositoryInterface";
 
-class ColecaoRepository implements ColecaoRepositoryInterface {
-  private readonly colecaoRepository: any;
-  constructor() {
-    this.colecaoRepository = AppDataSource.getRepository(ColecaoEntity)
+class ColecaoRepository implements ColecaoRepositoryInterface {/*Classe colecao repository, implementa o contrato com colecaoRepositoryInterface */
+  private readonly colecaoRepository: any; // cria uma variável para leitura que pode conter any types.
+  constructor() {//construtor de classe, utilizado para injecao de dependencias.
+    this.colecaoRepository = AppDataSource.getRepository(ColecaoEntity) 
   }
 
-  async findAll(titulo?: string): Promise<ColecaoEntity[]> {
+  async findAll(titulo?: string): Promise<ColecaoEntity[]> { // busca todos os registros de colecao, pode receber ou não um parametro
+    //busca todas as colecaoes no banco de dados
     let query = `
     SELECT
       colecao.id as colecao_id,
@@ -22,14 +23,15 @@ class ColecaoRepository implements ColecaoRepositoryInterface {
       colecao
   `;
 
-    if(titulo) {
+    if(titulo) {// se a funcao tiver sido solicitada com um parametro, ele irá procurar apenas colecoes com aquele titulo
       query += `WHERE colecao.titulo LIKE '%${titulo}%'`
     }
   
-    return this.colecaoRepository.query(query)
+    return this.colecaoRepository.query(query) // retorna a resposta do banco de dados
   }
 
-  async findById(id: number): Promise<ColecaoEntity | undefined> {
+  async findById(id: number): Promise<ColecaoEntity | undefined> { // funcao para buscar uma colecao em especifico 
+    //query para que a operacao seja realizada
     const query = `
     SELECT
       colecao.id as colecao_id,
@@ -43,26 +45,26 @@ class ColecaoRepository implements ColecaoRepositoryInterface {
     WHERE
       colecao.id = ${id}`;
 
-    const colecao =  await this.colecaoRepository.query(query)
+    const colecao =  await this.colecaoRepository.query(query) // recebe a resposta do banco de dados
 
-    return colecao.length ? colecao[0] : undefined
+    return colecao.length ? colecao[0] : undefined // verifica a resposta e envia, se tiver um registro, retorna ele, se não, retorna underfined
   }
 
-  async getColecaoOnly(id: number): Promise<ColecaoEntity> {
-    return this.colecaoRepository.findOne({ where: { id }})
+  async getColecaoOnly(id: number): Promise<ColecaoEntity> {//retrona apenas uma colecao
+    return this.colecaoRepository.findOne({ where: { id }})// retorna a resposta da funcao findOne, um registro contendo aquele ID
   }
 
-  async create (colecao: ColecaoInterface): Promise<ColecaoEntity> {
-    return this.colecaoRepository.save(colecao)
+  async create (colecao: ColecaoInterface): Promise<ColecaoEntity> {// Cria um novo registro no banco de dados
+    return this.colecaoRepository.save(colecao)// cria e em seguida retorna a resposta do método save.
   }
 
-  async update (id: number, colecao: ColecaoInterface): Promise<ColecaoEntity> {
-    await this.colecaoRepository.update(id, colecao)
-    return this.colecaoRepository.findOne({ where: { id }})
+  async update (id: number, colecao: ColecaoInterface): Promise<ColecaoEntity> {// atualiza um registro no banco de dados
+    await this.colecaoRepository.update(id, colecao)// realiza a atualizacao do registro
+    return this.colecaoRepository.findOne({ where: { id }})// retorna ao usuário qual registro foi atualizado
   }
 
-  async delete (id: number): Promise<void> {
-    await this.colecaoRepository.delete(id)
+  async delete (id: number): Promise<void> {// apaga um registro no banco de dados
+    await this.colecaoRepository.delete(id)// realiza a exclucao do registro
   }
 }
 
