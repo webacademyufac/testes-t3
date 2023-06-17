ec608404eccd89df5aa01a3ce78dca7cee50dfee
Kalebe silva <kalebe.silvadev@gmail.com>
2023-06-17 17:02:38 -0500

    Update routes.ts

--- codes/students/kalebesilva/backend_comentado/src/app/routes/routes.ts
+++ codes/students/kalebesilva/backend_comentado/src/app/routes/routes.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import ColecaoController from "../controllers/ColecaoController";
 import CitacaoController from "../controllers/CitacaoController";
 
-const routers = Router()
+const routers = Router() // Cria variável de rotas
 
-routers.use(ColecaoController.routes)
-routers.use(CitacaoController.routes)
+// informa quais rotas serao utilizadas na api
+routers.use(ColecaoController.routes) // usa a roda de colecao cotroller
+routers.use(CitacaoController.routes) // usa a rota de citacao controller
 
-export default routers
+export default routers //exporta para ser usada em outros lugares da aplicacao
