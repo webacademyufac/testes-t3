66c2aefdc3e9c87a37b322d9b97370c475d41a60
Kalebe silva <kalebe.silvadev@gmail.com>
2023-06-18 14:07:21 -0500

    comentário dos mocks

--- codes/students/kalebesilva/backend_comentado/src/tests/config/mocks/ColecaoRepositoryMock.ts
+++ codes/students/kalebesilva/backend_comentado/src/tests/config/mocks/ColecaoRepositoryMock.ts
@@ -1,8 +1,10 @@
 import { ColecaoRepositoryInterface } from "../../../app/interfaces/repositories/ColecaoRepositoryInterface";
 import { CreatedColecaoFixture, ManyColecaoFixture, OneColecaoFixture } from "../fixtures/ColecaoFixture";
 
-export const ColecaoRepositoryMock = (): jest.Mocked<ColecaoRepositoryInterface> => {
-  return {
+export const ColecaoRepositoryMock = (): jest.Mocked<ColecaoRepositoryInterface> => {/*mock serve para silmular as classes repository
+em nossos testes unitários, queremos isolar um componente, mas esse componente precisa acessar o banco de dados para funcionar, então,
+criamos uma simulacao do banco de dados, o coponente vai pensar que está acessando o banco, mas não estará. */
+  return {//returna os métodos básicos, de acordo com os que temos na terface colecaoRepository
     findAll: jest.fn().mockResolvedValue(ManyColecaoFixture),
     findById: jest.fn().mockResolvedValue(OneColecaoFixture),
     create: jest.fn().mockResolvedValue(CreatedColecaoFixture),
