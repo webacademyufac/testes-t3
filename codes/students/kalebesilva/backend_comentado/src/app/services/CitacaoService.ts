2346dbe8e40248aa788035e1eea6d0125c979ea9
Kalebe silva <kalebe.silvadev@gmail.com>
2023-06-18 23:26:25 -0500

    comentários de service

--- codes/students/kalebesilva/backend_comentado/src/app/services/CitacaoService.ts
+++ codes/students/kalebesilva/backend_comentado/src/app/services/CitacaoService.ts
@@ -6,81 +6,82 @@ import { ColecaoRepositoryInterface } from '../interfaces/repositories/ColecaoRe
 import CitacaoServiceInterface from '../interfaces/services/CitacaoServiceInterface';
 import 'express-async-errors';
 
-class CitacaoService implements CitacaoServiceInterface {
-  private readonly citacaoRepository: CitacaoRepositoryInterface;
+class CitacaoService implements CitacaoServiceInterface { 
+  
+  private readonly citacaoRepository: CitacaoRepositoryInterface; // cria objetos para leitura
   private readonly colecaoRepository: ColecaoRepositoryInterface;
 
   constructor(
-    citacaoRepository: CitacaoRepositoryInterface,
+    citacaoRepository: CitacaoRepositoryInterface, // parametros para o construtor
     colecaoRepository: ColecaoRepositoryInterface
   ) {
-    this.citacaoRepository = citacaoRepository;
+    this.citacaoRepository = citacaoRepository; // objetos da classe recebendo os objetos parametros do construtor
     this.colecaoRepository = colecaoRepository;
   }
 
-  async findAll(titulo?: string): Promise<CitacaoEntity[]> {
-    return await this.citacaoRepository.findAll(titulo);
+  async findAll(titulo?: string): Promise<CitacaoEntity[]> {// busca todos os objetos no banco de dados
+    return await this.citacaoRepository.findAll(titulo); // retorna a colecao retornada do BD
   }
 
-  async findById(id: number): Promise<CitacaoEntity> {
-    const citacao = await this.citacaoRepository.findById(id);
-    if (citacao) {
+  async findById(id: number): Promise<CitacaoEntity> { 
+    const citacao = await this.citacaoRepository.findById(id); // realiza a operacao
+    if (citacao) { //verifica se o resposta é existente ou não
       return citacao;
     }
-    throw new AppError({
+    throw new AppError({ // devolve uma mensagem 
       httpCode: HttpCode.NOT_FOUND,
       description: `Citacao com ID ${id} Não encontrada.`,
     });
   }
 
-  async create({
+  async create({ // funcao de insersao no BD
     titulo,
     id_colecao,
   }: CitacaoInterface): Promise<CitacaoEntity> {
-    return await this.citacaoRepository.create({ titulo, id_colecao });
+    return await this.citacaoRepository.create({ titulo, id_colecao }); // insere e retorna o obj criado
   }
 
-  async update(
+  async update( // atualiza um registro em especifico
     id: number,
     { titulo, id_colecao }: CitacaoInterface
   ): Promise<CitacaoEntity> {
-    const citacao = await this.citacaoRepository.getCitacaoOnly(id);
+    const citacao = await this.citacaoRepository.getCitacaoOnly(id); // verifica se o obj existe no banco de dados
 
-    if (!citacao) {
+    if (!citacao) { // se não existe, então devolve que não encontrou
       throw new AppError({
         httpCode: HttpCode.NOT_FOUND,
         description: `Citacao com ID ${id} Não encontrada.`,
       });
     }
 
-    if (titulo) {
+    if (titulo) { // verifica se a funcao foi passada com um titulo(já que ele é opcional)
       citacao.titulo = titulo;
     }
 
-    if (id_colecao) {
-      const colecao = await this.colecaoRepository.getColecaoOnly(id_colecao);
-      if (!colecao) {
+    if (id_colecao) { // verifica se a id foi passada como parametro 
+      const colecao = await this.colecaoRepository.getColecaoOnly(id_colecao); // verifica se aquele obj, com a id do parametro existe
+      if (!colecao) { //se não achar nada, então retorna que não achou a ID
         throw new AppError({
           httpCode: HttpCode.NOT_FOUND,
           description: `Colecao com ID ${id_colecao} Não encontrada.`,
         });
       }
-      citacao.id_colecao = id_colecao;
+      citacao.id_colecao = id_colecao; // se não retornar um erro, é pq existe, então irá atribuila ao obj
     }
 
-    return await this.citacaoRepository.update(id, citacao);
+    return await this.citacaoRepository.update(id, citacao); // retorna resultado do update, depois de fazer a opracao 
   }
 
-  async delete(id: number): Promise<void> {
-    const citacao = await this.citacaoRepository.getCitacaoOnly(id);
-    if (!citacao) {
+  async delete(id: number): Promise<void> { // deleta um registro
+    const citacao = await this.citacaoRepository.getCitacaoOnly(id);// verifica se o registro existe
+    if (!citacao) {// se não existir, retorna um erro de ID nao encontrada
       throw new AppError({
         httpCode: HttpCode.NOT_FOUND,
         description: `Citacao com ID ${id} Não encontrada.`,
       });
     }
-    await this.citacaoRepository.delete(id);
+    await this.citacaoRepository.delete(id); //realiza a exluisao do registro
   }
 }
 
-export default CitacaoService;
+export default CitacaoService;// exporta a classe para ser usada em outras partes da aplicacao
--- codes/students/kalebesilva/backend_comentado/src/app/services/ColecaoService.ts
+++ codes/students/kalebesilva/backend_comentado/src/app/services/ColecaoService.ts
@@ -6,34 +6,34 @@ import ColecaoServiceInterface from '../interfaces/services/ColecaoServiceInterf
 import ColecaoRepository from '../repositories/ColecaoRepository';
 
 class ColecaoService implements ColecaoServiceInterface {
-  private readonly colecaoRepository: ColecaoRepositoryInterface;
+  private readonly colecaoRepository: ColecaoRepositoryInterface; // cria obj de leitura colecaoRepository
 
-  constructor(colecaoRepository: ColecaoRepositoryInterface) {
+  constructor(colecaoRepository: ColecaoRepositoryInterface) { 
     this.colecaoRepository = colecaoRepository;
   }
 
-  async findAll(titulo?: string): Promise<ColecaoEntity[]> {
-    return await this.colecaoRepository.findAll(titulo);
+  async findAll(titulo?: string): Promise<ColecaoEntity[]> { // busca todos os objetos no banco de dados
+    return await this.colecaoRepository.findAll(titulo);// retorna a colecao retornada do BD
   }
 
   async findById(id: number): Promise<ColecaoEntity> {
-    const colecao = await this.colecaoRepository.findById(id);
-    if (colecao) {
+    const colecao = await this.colecaoRepository.findById(id); // realiza a operacao
+    if (colecao) { //verifica se colecao possui algum conteudo 
       return colecao;
     }
-    throw new AppError({
+    throw new AppError({ // não possuindo, retorna uma mensagem que a ID nao foi encontrada
       httpCode: HttpCode.NOT_FOUND,
       description: `Citacao com ID ${id} Não encontrada.`,
     });
   }
 
-  async create({
+  async create({ // cria uma nova colecao
     autor,
     titulo,
     subtitulo,
     imagem,
   }: ColecaoInterface): Promise<ColecaoEntity> {
-    return await this.colecaoRepository.create({
+    return await this.colecaoRepository.create({ // insere a colecao no BD
       autor,
       titulo,
       subtitulo,
@@ -41,19 +41,20 @@ class ColecaoService implements ColecaoServiceInterface {
     });
   }
 
-  async update(
+  async update( //Atualiza uma colecao
     id: number,
     { autor, titulo, subtitulo, imagem }: ColecaoInterface
   ): Promise<ColecaoEntity> {
-    const colecao = await this.colecaoRepository.getColecaoOnly(id);
+    const colecao = await this.colecaoRepository.getColecaoOnly(id); // verifica se encontra o registro
 
-    if (!colecao) {
-      throw new AppError({
+    if (!colecao) { // se o conteudo de colecao for vazio, entao o obj nao foi encontrado
+      throw new AppError({//devolve uma mensagem falando que a id nao foi achada
         httpCode: HttpCode.NOT_FOUND,
         description: `Colecao com ID ${id} Não encontrada.`,
       });
     }
 
+    // verifica se os campos passados no parametro possuem conteúdo, se sim, então atribuirá ao obj colecao
     if (autor) {
       colecao.autor = autor;
     }
@@ -70,19 +71,19 @@ class ColecaoService implements ColecaoServiceInterface {
       colecao.imagem = imagem;
     }
 
-    return await this.colecaoRepository.update(id, colecao);
+    return await this.colecaoRepository.update(id, colecao); // realiza o update e retorna a resposta do método
   }
 
-  async delete(id: number): Promise<void> {
-    const colecao = await this.colecaoRepository.getColecaoOnly(id);
-    if (!colecao) {
-      throw new AppError({
+  async delete(id: number): Promise<void> { // apaga um registro
+    const colecao = await this.colecaoRepository.getColecaoOnly(id); // verifica se o obj existe
+    if (!colecao) {// se o conteudo de colecao for vazio, entao a busca nao encontrou nada
+      throw new AppError({ // devolve uma mensagem de id nao encontrada
         httpCode: HttpCode.NOT_FOUND,
         description: `Colecao com ID ${id} Não encontrada.`,
       });
     }
-    return await this.colecaoRepository.delete(id);
+    return await this.colecaoRepository.delete(id); //realiza a exclusao do registro 
   }
 }
 
-export default ColecaoService;
+export default ColecaoService; // exporta a classe para ser usada em outras partes da aplicacao
